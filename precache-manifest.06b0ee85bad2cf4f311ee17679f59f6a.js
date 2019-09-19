6b57a6f3e52f4ae1aaca7124b0873bd03cd4e5e9
hong0311 <ddyydonaldyau@gmail.com>
2019-09-19 16:11:01 +0800

    Updates

---
+++ precache-manifest.06b0ee85bad2cf4f311ee17679f59f6a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb5076111b9ea26e9dab21490e9f84be",
+    "url": "/css-to-material-ui-translator/index.html"
+  },
+  {
+    "revision": "1cc3649d0620597f5f2a",
+    "url": "/css-to-material-ui-translator/static/css/main.ba49ec77.chunk.css"
+  },
+  {
+    "revision": "b629b40ccbdb4f947fd4",
+    "url": "/css-to-material-ui-translator/static/js/2.f0ed2185.chunk.js"
+  },
+  {
+    "revision": "1cc3649d0620597f5f2a",
+    "url": "/css-to-material-ui-translator/static/js/main.74916028.chunk.js"
+  },
+  {
+    "revision": "f230960a43fad0d8c2ac",
+    "url": "/css-to-material-ui-translator/static/js/runtime~main.077f2fba.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/css-to-material-ui-translator/static/media/logo.ee7cd8ed.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/css-to-material-ui/precache-manifest.6d26b4fe0f38dafebd5d64dd5aefa8c5.js"
+  "/css-to-material-ui-translator/precache-manifest.06b0ee85bad2cf4f311ee17679f59f6a.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/css-to-material-ui/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/css-to-material-ui-translator/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
